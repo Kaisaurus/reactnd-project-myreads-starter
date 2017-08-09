693a086150410665128e18218260d9e68f9b3cb2
Kaisaurus <kaigotoh@gmail.com>
2017-08-09 20:07:20 +0200

    all basic functionality working

--- src/App.js
+++ src/App.js
@@ -4,35 +4,62 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import SearchBooks from './SearchBooks';
 import Header from './Header';
-import Sample from './Sample';
 import ListBooks from './ListBooks';
 import AlertContainer from 'react-alert';
 
 class BooksApp extends React.Component {
   state = {
-    books: [],
+    myBooks: [],
+    searchResult: [],
+    noSearchResultMsg: true,
   }
 
   componentDidMount() {
     // loads all books
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books });
-    });
+    BooksAPI.getAll()
+      .then(myBooks => this.setState({ myBooks }))
+      .catch((err) => {
+        this.showAlert(`Wops.. books didn't load. Error: ${err.message}`);
+      });
   }
 
-  onShelfChange = (bookId, shelf) => {
-    const changedBook = this.state.books.find((book) => book.id === bookId);
+  onSearchBooks = (query) => {
+    // empty search results if query is empty
+    if (!query) {
+      this.setState({ searchResult: [] });
+    } else {
+      // Search for books
+      BooksAPI.search(query, 20)
+        .then(searchResult => {
+          if (searchResult.length >= 1) {
+            this.setState({ searchResult });
+            this.setState({ noSearchResultMsg: true });
+          } else {
+            this.setState({ searchResult: [] });
+            this.setState({ noSearchResultMsg: false });
+          }
+        })
+        .catch((err) => {
+          this.showAlert(`Wops.. something went wrong with the search. Error: ${err.message}`);
+        });
+    }
+  }
+
+  onShelfChange = (changedBook, shelf) => {
     BooksAPI
       .update(changedBook, shelf)
       .then(() => {
-        const newBooks = this.state.books.map((book) => {
-          const newBook = book;
-          if (book.id === bookId) {
-            newBook.shelf = shelf;
-          }
-          return newBook;
-        });
-        this.setState({ books: newBooks });
+        // filter myBooks to exclude the modified book
+        const newMyBooks = this.state.myBooks.filter(book =>
+          book.id !== changedBook.id
+        );
+        // add the changed book to newMyBooks if a shelf is set
+        if (shelf !== 'none') {
+          const updatedBook = changedBook;
+          updatedBook.shelf = shelf;
+          newMyBooks.push(updatedBook);
+        }
+        this.setState({ myBooks: newMyBooks });
       })
       .then(() => this.showAlert(`${changedBook.title} moved to ${shelf}`))
       .catch((err) => {
@@ -55,14 +82,24 @@ class BooksApp extends React.Component {
   }
 
   render() {
-    const { books } = this.state;
+    const { myBooks, searchResult, noSearchResultMsg } = this.state;
     return (
       <div className="app">
         <Header />
-        <Route path="/search" component={ SearchBooks } />
-        <Route path="/sample" component={ Sample } />
+        <Route path="/search" render={() => (
+            <SearchBooks
+              onSearchBooks={ this.onSearchBooks }
+              onShelfChange={ this.onShelfChange }
+              searchResult={ searchResult }
+              noSearchResultMsg={ noSearchResultMsg }
+            />
+          )}
+        />
         <Route exact path="/" render={() => (
-            <ListBooks books={ books } onShelfChange={ this.onShelfChange } />
+            <ListBooks
+              books={ myBooks }
+              onShelfChange={ this.onShelfChange }
+            />
           )}
         />
         <AlertContainer
